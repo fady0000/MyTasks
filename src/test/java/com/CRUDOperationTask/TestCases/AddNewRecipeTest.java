553f5f275d979bed29b9925e6e43e8fb7766c443
Fady Michel <fadymichel93@gmail.com>
2021-06-07 18:25:41 +0200

    Commit

--- src/test/java/com/CRUDOperationTask/TestCases/AddNewRecipeTest.java
+++ src/test/java/com/CRUDOperationTask/TestCases/AddNewRecipeTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class AddNewRecipeTest extends TestBaseClass {
     private String Recipe = "New Recipe";
-    private String Ingredients = "ingred 1 , ingred 2, ingred 3";
+    private String Ingredients = "ingred 1 , ingred 2, ingred 3 , ingred 4";
 
     @Test
     public void AddNewRecap() throws InterruptedException {
